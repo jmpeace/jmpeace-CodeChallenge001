ee11ae57b224e17b57d0850c61471f996aa9e211
jmpeace <jmpeace@gmail.com>
2020-09-03 12:11:00 -0400

    end of coding for example purposes

--- src/main/java/com/dharbortest/controller/StudentController.java
+++ src/main/java/com/dharbortest/controller/StudentController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dharbortest.dao.StudentRepo;
@@ -41,7 +43,11 @@ public class StudentController {
 	}
 	
 	@GetMapping(value = "/{id}")
-	public Optional<Student> list(@PathVariable("id") Integer id){
+	public Optional<Student> list(@PathVariable("id") Integer id) throws Exception {
+		Optional<Student> student = repo.findById(id); 
+		
+		if(!student.isPresent()) throw new Exception("No such student.");
+		
 		return repo.findById(id);
 	}
 	
